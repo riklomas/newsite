ff797f184d8d9d983325fdf5a1d277ba675c2776
mebiwa <meganbingham@hotmail.com>
2013-12-10 14:20:38 +0000

    added first post_image via form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,16 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(post_params)
+			redirect_to post_path(@post)
+		else
+			render :edit
+		end
+	end
+
 	def post_params
 		params.require(:post).permit(:post_image)
 	end
