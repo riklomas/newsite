454ebf6d9c626c6a74ca160e734e09ce00fb2bfa
mebiwa <meganbingham@hotmail.com>
2013-12-10 10:48:45 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Newsite::Application.config.secret_key_base = 'aa9dc870ffbd954c8de92ed9b2e45bdc573a74ff1a71ffbb453d3c161792b5f202f779f8e37901b9e0f770c1e3e48668852932eb107d882faf93230eb8d00f56'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Newsite::Application.config.session_store :cookie_store, key: '_newsite_session'
