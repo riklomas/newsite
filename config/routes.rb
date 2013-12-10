65ecb572d515da6225a164e1caa98ed5b202a2fd
mebiwa <meganbingham@hotmail.com>
2013-12-10 15:27:11 +0000

    integrated index page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Newsite::Application.routes.draw do
   resources :posts do
   end
 
-   root "posts#index"
+   root "contents#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
